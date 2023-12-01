d0a1c92fd1a035d4e00e863edd0889d639e952c0
Emma <emmathornewell@gmail.com>
2023-12-01 14:17:04 -0500

    push pop and front implimented

---
+++ GAME2001_A2_Emma_Thornewell/LinkedList.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "LinkedList.h"
+
+template <typename T>
+void LinkedList<T>::Push(int priority, const T& data)
+{
+	Mode<T>* newNode = new Node<T>(priority, data);
+	newNode->SetNextNode(head);
+	SetHead(newNode);
+	SetSize(GetSize() + 1);
+}
+
+template <typename T>
+void LinkedList<T>::Pop()
+{
+	if (GetHead()) 
+	{
+		Node<T>* temp = GetHead();
+		SetHead(temp->GetNextNode());
+		delete temp;
+
+		SetSize(GetSize() - 1);
+	}
+}
