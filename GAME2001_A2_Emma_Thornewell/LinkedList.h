ab9a70798e27678ac47d58bbd4cb2869124b7480
Emma <emmathornewell@gmail.com>
2023-12-01 14:14:04 -0500

    setting up the lihked list... not quite done yet though

--- GAME2001_A2_Emma_Thornewell/LinkedList.h
+++ GAME2001_A2_Emma_Thornewell/LinkedList.h
@@ -12,6 +12,33 @@ public:
 	~LinkedList();
 
 
+	//adding getters and setters for the linked list
+	Node<T>* GetHead() const
+	{
+		return head;
+	}
+
+	void SetHead(Node<T>* newHead)
+	{
+		head = newHead;
+	}
+
+	int GetSize() const
+	{
+		return size;
+	}
+
+	void SetSize(int newSize)
+	{
+		size = newSize;
+	}
+
+	//methods
+	void Push(int priority, const T& data);
+	void Pop();
+	Node<T>* Front() const;
+
+
 private:
 	Node<T>* head;
 	int size;
